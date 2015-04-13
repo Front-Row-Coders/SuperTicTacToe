8e4172fe5d8295e6eabd594e191c95498731fb85
Jonathan <>
2015-04-13 11:31:03 -0500

    Initial Location class setup.

---
+++ src/Location.java
@@ -0,0 +1,26 @@
+/**
+ *
+ * @author Jonathan
+ * @version 1.0
+ */
+public class Location
+{
+	private final int xPos;
+	private final int yPos;
+	
+	public Location(int x, int y)
+	{
+		this.xPos = x;
+		this.yPos = y;
+	}
+	
+	public int getXPos()
+	{
+		return this.xPos;
+	}
+	
+	public int getYPos()
+	{
+		return this.yPos;
+	}
+}
