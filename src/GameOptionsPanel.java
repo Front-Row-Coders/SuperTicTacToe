ff87cc6e5f91dc6ddc9335fa14c9ccd85e1f9a74
Jonathan <generalnova92@gmail.com>
2015-04-16 15:49:31 -0500

    Revised package name.

--- src/GameOptionsPanel.java
+++ src/GameOptionsPanel.java
@@ -1,20 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package SuperTicTacToe.src;
 
 /**
  *
  * @author alice
  */
-public class GameOptionsPanel extends javax.swing.JPanel {
+public class GameOptionsPanel extends javax.swing.JPanel
+{
+	private static final long serialVersionUID = 1L;
 
     /**
      * Creates new form RegisterFormPanel
      */
-    public GameOptionsPanel() {
+    public GameOptionsPanel()
+	{
         initComponents();
     }
 
