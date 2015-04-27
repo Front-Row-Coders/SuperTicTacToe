e4dc327ee8e759d2f72ed0d8a0dda111af1b3629
Jonathan <generalnova92@gmail.com>
2015-04-27 15:56:01 -0500

    Integrated Multiplayer class to work with system
    Added validation code on submit, if valid, creates a new game with
    selected options. If not, displays an appropriate error message.

--- src/Multiplayer.java
+++ src/Multiplayer.java
@@ -1,15 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package SuperTicTacToe.src;
+
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Eloy
  */
-public class Multiplayer extends javax.swing.JPanel {
+public class Multiplayer extends UIPanel {
+	private static final long serialVersionUID = 1L;
 
     /**
      * Creates new form Multiplayer
@@ -25,7 +22,8 @@ public class Multiplayer extends javax.swing.JPanel {
      */
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
+    private void initComponents()
+    {
 
         firstPlayer = new javax.swing.ButtonGroup();
         p1RadioButton = new javax.swing.JRadioButton();
@@ -40,9 +38,12 @@ public class Multiplayer extends javax.swing.JPanel {
         playButton = new javax.swing.JButton();
 
         firstPlayer.add(p1RadioButton);
+        p1RadioButton.setSelected(true);
         p1RadioButton.setText("Player 1");
-        p1RadioButton.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+        p1RadioButton.addActionListener(new java.awt.event.ActionListener()
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
                 p1RadioButtonActionPerformed(evt);
             }
         });
@@ -52,30 +53,38 @@ public class Multiplayer extends javax.swing.JPanel {
 
         p1NameLabel.setText("Player 1 username:");
 
-        p2NameTxtField.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+        p2NameTxtField.addActionListener(new java.awt.event.ActionListener()
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
                 p2NameTxtFieldActionPerformed(evt);
             }
         });
 
         p1GuestCheckBox.setText("Guest");
-        p1GuestCheckBox.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+        p1GuestCheckBox.addActionListener(new java.awt.event.ActionListener()
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
                 p1GuestCheckBoxActionPerformed(evt);
             }
         });
 
         p2NameLabel.setText("Player 2 username:");
 
-        p1NameTxtField.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+        p1NameTxtField.addActionListener(new java.awt.event.ActionListener()
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
                 p1NameTxtFieldActionPerformed(evt);
             }
         });
 
         p2GuestCheckBox.setText("Guest");
-        p2GuestCheckBox.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+        p2GuestCheckBox.addActionListener(new java.awt.event.ActionListener()
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
                 p2GuestCheckBoxActionPerformed(evt);
             }
         });
@@ -83,6 +92,13 @@ public class Multiplayer extends javax.swing.JPanel {
         firstPlayerLabel.setText("First player:");
 
         playButton.setText("Play Game");
+        playButton.addActionListener(new java.awt.event.ActionListener()
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                playButtonActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -139,7 +155,7 @@ public class Multiplayer extends javax.swing.JPanel {
                             .addComponent(firstPlayerLabel))))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(playButton)
-                .addContainerGap(7, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
@@ -155,7 +171,7 @@ public class Multiplayer extends javax.swing.JPanel {
         // TODO add your handling code here:
         p1NameTxtField.setEditable(!p1NameTxtField.isEditable());
         if(!p1NameTxtField.isEditable())
-        { p1NameTxtField.setText("Guest");} //reference later
+        { p1NameTxtField.setText(Game.GUEST_NAME);} //reference later
         else
         {p1NameTxtField.setText("");}
     }//GEN-LAST:event_p1GuestCheckBoxActionPerformed
@@ -168,11 +184,64 @@ public class Multiplayer extends javax.swing.JPanel {
         // TODO add your handling code here:
         p2NameTxtField.setEditable(!p2NameTxtField.isEditable());
         if(!p2NameTxtField.isEditable())
-        {p2NameTxtField.setText("Guest");}//reference later
+        {p2NameTxtField.setText(Game.GUEST_NAME);}//reference later
         else
         {p2NameTxtField.setText("");}
 
     }//GEN-LAST:event_p2GuestCheckBoxActionPerformed
+
+    private void playButtonActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_playButtonActionPerformed
+    {//GEN-HEADEREND:event_playButtonActionPerformed
+        String p1 = this.p1NameTxtField.getText().trim();
+		String p2 = this.p2NameTxtField.getText().trim();
+		if(p1.length() <= 0)
+		{
+			JOptionPane.showMessageDialog(this, "Empty player one username value."
+				, "Invalid", JOptionPane.ERROR_MESSAGE);
+			return;
+		}
+		if(p2.length() <= 0)
+		{
+			JOptionPane.showMessageDialog(this, "Empty player two username value."
+				, "Invalid", JOptionPane.ERROR_MESSAGE);
+			return;
+		}
+		
+		if(p1.equals(Game.AI_NAME) || (!this.p1GuestCheckBox.isSelected() && p1.equals(Game.GUEST_NAME)))
+		{
+			JOptionPane.showMessageDialog(this, "Invalid username one due to reserved name."
+				, "Invalid", JOptionPane.ERROR_MESSAGE);
+			return;
+		}
+		if(p2.equals(Game.AI_NAME) || (!this.p2GuestCheckBox.isSelected() && p2.equals(Game.GUEST_NAME)))
+		{
+			JOptionPane.showMessageDialog(this, "Invalid username two due to reserved name."
+				, "Invalid", JOptionPane.ERROR_MESSAGE);
+			return;
+		}
+		
+		if(!this.p1GuestCheckBox.isSelected())
+		{
+			if(UIWindow.getHistoryInstance().getPlayer(p1) == null)
+			{
+				JOptionPane.showMessageDialog(this, "Username One does not exist."
+				, "Invalid", JOptionPane.ERROR_MESSAGE);
+				return;
+			}
+		}
+		
+		if(!this.p2GuestCheckBox.isSelected())
+		{
+			if(UIWindow.getHistoryInstance().getPlayer(p2) == null)
+			{
+				JOptionPane.showMessageDialog(this, "Username Two does not exist."
+				, "Invalid", JOptionPane.ERROR_MESSAGE);
+				return;
+			}
+		}
+		
+		UIWindow.getInstance().setCurrentPanel(new GridPanel(p1, p2, this.p1RadioButton.isSelected()));
+    }//GEN-LAST:event_playButtonActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
